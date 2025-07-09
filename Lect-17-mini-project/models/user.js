33af1b637e6c1686ce10a6d376da009eeb338f74
suman <sumankumarghosh94@gmail.com>
2025-07-09 23:53:56 +0530

    add mini-project-part-1

---
+++ Lect-17-mini-project/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/miniproject");
+
+const userSchema = mongoose.Schema({
+  username:String,
+  name: String,
+  age:Number,
+  email:String,
+  password: String,
+  posts : [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+})
+
+module.exports = mongoose.model('User', userSchema);
